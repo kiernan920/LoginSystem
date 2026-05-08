96aaf91dd4ced7778827aeca24fa3afbd8d7162c
J K <kiernan920@gmail.com>
2026-05-08 17:01:49 +0100

    Redirect Working

--- src/main/java/com/example/demo/appuser/AppUserRepository.java
+++ src/main/java/com/example/demo/appuser/AppUserRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo.appuser;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +8,6 @@ import java.util.Optional;
 
 @Repository
 @Transactional(readOnly = true)
-public interface AppUserRepository {
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
     Optional<AppUser> findByEmail(String email);
 }
--- src/main/java/com/example/demo/registration/RegistrationController.java
+++ src/main/java/com/example/demo/registration/RegistrationController.java
@@ -1,6 +1,8 @@
 package com.example.demo.registration;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/registration")
 @AllArgsConstructor
 public class RegistrationController {
+
+    private RegistrationService registrationService;
+
+    @PostMapping
+    public String register(@RequestBody RegistrationRequest request) {
+        return registrationService.register(request);
+    }
 }
